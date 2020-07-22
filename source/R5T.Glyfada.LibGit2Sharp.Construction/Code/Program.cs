2380842ed34ac03faf61887a2591b2c2981ff680
MinexAutomation <minex.automation@gmail.com>
2020-07-21 18:22:50 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Glyfada.LibGit2Sharp.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Glyfada.LibGit2Sharp.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Glyfada.LibGit2Sharp/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Glyfada.LibGit2Sharp
+{
+    public class Class1
+    {
+    }
+}
